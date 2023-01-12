199a1e5ac51abca4f1a912a95d778a38cb32f7b3
ezdar <ezdar2743@gmail.com>
2023-01-12 09:53:24 +0900

    resolve conflict

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,31 +4,25 @@ class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'Expample', email: 'user@example.com', password: 'foobar', password_confirmation: 'foobar')
   end
-  
   test 'should be valid' do
     assert @user.valid?
   end
-
   test 'name should be present' do
     @user.name = '  '
     assert_not @user.valid?
   end
-
   test 'email should be present' do
     @user.email = '      '
     assert_not @user.valid?
   end
-
   test 'name should not be too long' do
     @user.name = 'a' * 11
     assert_not @user.valid?
   end
-
   test 'email should not be too long' do
     @user.email = 'a' * 21
     assert_not @user.valid?
   end
-
   test 'email validation should accept valid address' do
     valid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
     valid_addresses.each do |valid_address|
@@ -36,13 +30,11 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{valid_address.inspect} should be invalid"
     end
   end
-
   test 'email addresses should be unique' do
     duplicate_user = @user.dup
     @user.save
     assert_not duplicate_user.valid?
   end
-
   test 'password should be present (nonblank)' do
     @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
