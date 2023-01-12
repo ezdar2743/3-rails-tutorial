9d124c5d3f989f73876121e774d3c986d3a85101
ezdar <ezdar2743@gmail.com>
2023-01-12 13:25:42 +0900

    create initial create controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,3 +1,11 @@
 class SessionController < ApplicationController
   def new; end
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      
+    else 
+      render 'new',status: :unprocessable_entity
+    end
+  end
 end
