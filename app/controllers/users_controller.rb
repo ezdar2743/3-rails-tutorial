16ed95ba52a9e26058adab996e5c126b1d3034f7
ezdar <ezdar2743@gmail.com>
2023-01-12 10:11:51 +0900

    apply rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @user= User.new
+    @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
@@ -19,6 +20,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,28 +1,27 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
     end
     assert_response :unprocessable_entity
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
-  test "valid signup information" do
-    assert_difference 'User.count',1 do
-      post users_path, params: {user:{
-        name:"Example User",
-        email:"user@example.com",
-        password:"1234",
-        password_confirmation:"1234"
-      }}
+  test 'valid signup information' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        name: 'Example User',
+        email: 'user@example.com',
+        password: '1234',
+        password_confirmation: '1234'
+      } }
     end
     follow_redirect!
     assert_template 'users/show'
