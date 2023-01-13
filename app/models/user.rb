a4d7ca068e6b8c243d9cbbc499660796e19afb0f
ezdar <ezdar2743@gmail.com>
2023-01-13 16:55:58 +0900

    gather validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 10 }
+  has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 10 }
   validates :email, presence: true, length: { maximum: 20 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 end
