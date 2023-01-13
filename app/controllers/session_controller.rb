8369fa3f1a1fdab73099039a6b9b38be96131ad1
ezdar <ezdar2743@gmail.com>
2023-01-13 17:01:41 +0900

    add login function

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
   def new; end
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
       redirect_to user
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,10 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 20 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 3 }
+    # 渡された文字列のハッシュ値を返す
+    def User.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
 end
