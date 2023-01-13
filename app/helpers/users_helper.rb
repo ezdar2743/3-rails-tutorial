d62f73933f1d6d963e7edf2ccbe17c17002e3c69
ezdar <ezdar2743@gmail.com>
2023-01-13 17:34:04 +0900

    delete no need message

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,4 @@
 module UsersHelper
-<<<<<<< HEAD
-  # 引数で与えられたユーザーのGravatar画像を返す
-  def gravatar_for(user, size: 40)
-    gravatar_id  = Digest::MD5.hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
-  end
-end
-=======
-
     # 引数で与えられたユーザーのGravatar画像を返す
     def gravatar_for(user)
       gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
@@ -16,4 +6,3 @@ end
       image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
   end
->>>>>>> 35267f6 (change validation and add gravatar)
