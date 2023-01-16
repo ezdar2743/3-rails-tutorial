2718747ce77f77fef85d0dff4be46ff2697823d2
ezdar <ezdar2743@gmail.com>
2023-01-16 16:23:48 +0900

    add cookies

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,7 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       reset_session
+      remember user
       log_in user
       redirect_to user
     else 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { email.downcase! }
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,8 +9,25 @@ class User < ApplicationRecord
                     uniqueness: true
   validates :password, presence: true, length: { minimum: 3 }
     # 渡された文字列のハッシュ値を返す
-    def User.digest(string)
+   class << self
+    def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
       BCrypt::Password.create(string, cost: cost)
     end
+
+    #random token 
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+   end
+
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+      # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
