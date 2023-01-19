0eb4c34dde9c51582e1d6bfe765fbf5ba698c802
ezdar <ezdar2743@gmail.com>
2023-01-19 15:16:01 +0900

    fixed bugs,test

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -13,7 +13,7 @@ class SessionController < ApplicationController
     end
   end
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url, status: :see_other
   end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,14 +11,14 @@ class InvalidPasswordTest < UsersLogin
 
   test "login path" do
     get login_path
-    assert_template 'sessions/new'
+    assert_template 'session/new'
   end
 
   test "login with valid email/invalid password" do
     post login_path, params: { session: { email:    @user.email,
                                           password: "invalid" } }
     assert_not is_logged_in?
-    assert_template 'sessions/new'
+    assert_template 'session/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -47,6 +47,12 @@ class ValidLoginTest < ValidLogin
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
+    delete logout_path
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
 
@@ -71,5 +77,10 @@ class LogoutTest < Logout
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "should still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
   end
 end
