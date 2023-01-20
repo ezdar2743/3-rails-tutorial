bd9e503c1a2974cdd83458864ade312b923b9601
ezdar <ezdar2743@gmail.com>
2023-01-20 17:13:28 +0900

    refactore form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,11 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user.update(user_params)
+      # 更新に成功した場合を扱う
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   private
