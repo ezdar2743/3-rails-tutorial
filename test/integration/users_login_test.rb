ca6c5ca589c8825da98022ea6fb7e5422ad9b603
ezdar <ezdar2743@gmail.com>
2023-01-27 17:40:56 +0900

    refactore test, user.rb

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,36 +1,24 @@
-require 'test_helper'
+require "test_helper"
 
 class UsersLogin < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:michael)
-  end
-
-  test 'login with remembering' do
-    log_in_as(@user, remember_me: '1')
-    assert_equal cookies[:remember_token], assigns(:user).remember_token
-    assert_not cookies[:remember_token].blank?
-  end
-
-  test 'login without remembering' do
-    # Cookieを保存してログイン
-    log_in_as(@user, remember_me: '1')
-    # Cookieが削除されていることを検証してからログイン
-    log_in_as(@user, remember_me: '0')
-    assert cookies[:remember_token].blank?
   end
 end
 
 class InvalidPasswordTest < UsersLogin
-  test 'login path' do
+
+  test "login path" do
     get login_path
-    assert_template 'session/new'
+    assert_template 'sessions/new'
   end
 
-  test 'login with valid email/invalid password' do
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'invalid' } }
+  test "login with valid email/invalid password" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
     assert_not is_logged_in?
-    assert_template 'session/new'
+    assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
@@ -38,35 +26,32 @@ class InvalidPasswordTest < UsersLogin
 end
 
 class ValidLogin < UsersLogin
+
   def setup
     super
-    post login_path, params: { session: { email: @user.email,
+    post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
   end
 end
 
 class ValidLoginTest < ValidLogin
-  test 'valid login' do
+
+  test "valid login" do
     assert is_logged_in?
     assert_redirected_to @user
   end
 
-  test 'redirect after login' do
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
-    assert_select 'a[href=?]', login_path, count: 0
-    assert_select 'a[href=?]', logout_path
-    assert_select 'a[href=?]', user_path(@user)
-    # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
-    delete logout_path
-    follow_redirect!
-    assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', logout_path,      count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
 
 class Logout < ValidLogin
+
   def setup
     super
     delete logout_path
@@ -74,21 +59,38 @@ class Logout < ValidLogin
 end
 
 class LogoutTest < Logout
-  test 'successful logout' do
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
   end
 
-  test 'redirect after logout' do
+  test "redirect after logout" do
     follow_redirect!
-    assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', logout_path,      count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
-  test 'should still work after logout in second window' do
+  test "should still work after logout in second window" do
     delete logout_path
     assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
