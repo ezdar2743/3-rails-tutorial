a9b0c74da32ce80108e446774bcfb706d77dc3e2
ezdar <ezdar2743@gmail.com>
2023-01-27 17:43:34 +0900

    make test failed to pass

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,14 +11,14 @@ class InvalidPasswordTest < UsersLogin
 
   test "login path" do
     get login_path
-    assert_template 'sessions/new'
+    assert_template 'session/new'
   end
 
   test "login with valid email/invalid password" do
     post login_path, params: { session: { email:    @user.email,
                                           password: "invalid" } }
     assert_not is_logged_in?
-    assert_template 'sessions/new'
+    assert_template 'session/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
