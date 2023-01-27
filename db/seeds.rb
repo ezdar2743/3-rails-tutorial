a3bdd9681bf3c93d43fe168ea794220c58e7762d
ezdar <ezdar2743@gmail.com>
2023-01-27 11:49:07 +0900

    update seeds,users.yml files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,21 @@
 # メインのサンプルユーザーを1人作成する
 User.create!(name: 'Ezdar',
-             email: 'ezdar2743@gmail.com',
-             password: '1234',
-             password_confirmation: '1234',
-             admin: true)
+            email: 'ezdar2743@gmail.com',
+            password: '1234',
+            password_confirmation: '1234',
+            admin: true,
+            activated: true,
+            activated_at: Time.zone.now)
 
 # 追加のユーザーをまとめて生成する
 99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n + 1}@railstutorial.org"
-  password = 'password'
-  User.create!(name:,
-               email:,
-               password:,
-               password_confirmation: password)
+    name  = Faker::Name.name
+    email = "example-#{n + 1}@railstutorial.org"
+    password = 'password'
+    User.create!(name: name,
+                email: email,
+                password: password,
+                password_confirmation: password,
+                activated: true,
+                activated_at: Time.zone.now)
 end
