1b9cb03010467d3015497fd253d9b90f1abe625a
ezdar <ezdar2743@gmail.com>
2023-01-30 00:46:46 +0900

    adjust rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
+
   before_save :downcase_email
   before_create :create_activation_digest
-  
+
   has_secure_password
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 30 }
@@ -39,6 +40,7 @@ class User < ApplicationRecord
   def authenticated?(attribute, token)
     digest = send("#{attribute}_digest")
     return false if digest.nil?
+
     BCrypt::Password.new(digest).is_password?(token)
   end
 
@@ -46,7 +48,8 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
-   # アカウントを有効にする
+
+  # アカウントを有効にする
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
@@ -56,10 +59,9 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
 
-
   private
 
-  #Email to Lowercase
+  # Email to Lowercase
   def downcase_email
     email.downcase!
   end
@@ -68,7 +70,5 @@ class User < ApplicationRecord
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
-    
   end
-
 end
