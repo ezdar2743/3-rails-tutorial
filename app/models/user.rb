b2c03f3d360e02fc9b08eb4b3b7c0147ac718278
ezdar <ezdar2743@gmail.com>
2023-02-02 10:29:06 +0900

    connect user to microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   before_create :create_activation_digest
 
   has_secure_password
+  has_many :microposts
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 30 },
