691ae9924d7b3f78764062c6b3346837f1be2556
ezdar <ezdar2743@gmail.com>
2023-02-03 16:44:01 +0900

    test follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,13 @@ class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id",
   dependent:   :destroy
+
   has_many :passive_relationships, class_name:  "Relationship",foreign_key: "followed_id",
   dependent:   :destroy
+
   has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-  
+  has_many :followers, through: :passive_relationships#省略ok, source: :follower
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 30 },
